712c9956221cc57aaf1a29b48278375af59543da
eliess <eloykreir85@gmail.com>
2026-05-30 11:06:38 +0200

    service side finished

--- src/main/java/fr/devisgenerator/devisgenerator/repository/QuoteRepository.java
+++ src/main/java/fr/devisgenerator/devisgenerator/repository/QuoteRepository.java
@@ -4,9 +4,11 @@ import fr.devisgenerator.devisgenerator.entity.Quote;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface QuoteRepository extends JpaRepository<Quote, Long> {
 
     List<Quote> findByUser_Id(Long userId);
+    Optional<Quote> findTopByOrderByIdDesc();
 
 }
